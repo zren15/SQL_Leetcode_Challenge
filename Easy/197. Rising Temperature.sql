1a71dc8a80c1ab0ddca1a9e9ad22e000b659cb93
Renne <Renne@RennedeMacBook-Pro.local>
2022-01-27 09:27:52 -0800

    commit 1/26

---
+++ Easy/197. Rising Temperature.sql
@@ -0,0 +1,64 @@
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+
+# Write your MySQL query statement below
+# 自己写的，存在特定bug
+select id from 
+(select id, temperature,
+       Lag(temperature,1)OVER(order by id) as num_1   #选择指定行的向后一行的内容
+from weather
+    ) A
+where A.temperature > A.num_1
+
+
+# 好的solution
+SELECT
+    weather.id AS Id
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+        AND weather.Temperature > w.Temperature
+;
